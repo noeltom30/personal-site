05b110529be70dcf8727427802956bd699db16df
noeltom30 <noel.2005.tom@gmail.com>
2025-04-03 18:53:50 +0530

    api gods please make this work

--- src/pages/api/lastfmapi.ts
+++ src/pages/api/lastfmapi.ts
@@ -1,26 +1,26 @@
-export async function GET(){
-    const API_URL = "https://ws.audioscrobbler.com/2.0/";
-    const API_KEY = import.meta.env.LASTFM_API_KEY;    
-    const url = `${API_URL}?method=user.getrecenttracks&user=noel_30_tom&api_key=${API_KEY}&format=json&limit=1`;
-    try{
-        // const response = await fetch(url,{
-        //     method: "GET",
-        //     headers: {                
-        //         "User-Agent":"personalwebsite/1.0"
-        //     }
-        // }
-        // );
-        const response = await fetch(url);
-        if(!response.ok){
-            console.error(`Error: ${response.status} - ${response.statusText}`)
-            throw new Error(`Failed to fetch: ${response.statusText}`);            
-        }
-        return response;
-    }
-    catch(error){
-        return new Response(JSON.stringify({error: error.message}), {
-            status: 500,
-            headers: {"Content-Type":"application/json"},
-        });
-    }
-}
+// export async function GET(){
+//     const API_URL = "https://ws.audioscrobbler.com/2.0/";
+//     const API_KEY = import.meta.env.LASTFM_API_KEY;    
+//     const url = `${API_URL}?method=user.getrecenttracks&user=noel_30_tom&api_key=${API_KEY}&format=json&limit=1`;
+//     try{
+//         // const response = await fetch(url,{
+//         //     method: "GET",
+//         //     headers: {                
+//         //         "User-Agent":"personalwebsite/1.0"
+//         //     }
+//         // }
+//         // );
+//         const response = await fetch(url);
+//         if(!response.ok){
+//             console.error(`Error: ${response.status} - ${response.statusText}`)
+//             throw new Error(`Failed to fetch: ${response.statusText}`);            
+//         }
+//         return response;
+//     }
+//     catch(error){
+//         return new Response(JSON.stringify({error: error.message}), {
+//             status: 500,
+//             headers: {"Content-Type":"application/json"},
+//         });
+//     }
+// }
