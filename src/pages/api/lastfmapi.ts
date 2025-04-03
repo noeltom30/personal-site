d0dea7ebc781123a3f3649a3560cc16f7c7a3f6b
noeltom30 <noel.2005.tom@gmail.com>
2025-04-03 13:44:02 +0530

    added headers to api usage

--- src/pages/api/lastfmapi.ts
+++ src/pages/api/lastfmapi.ts
@@ -3,7 +3,14 @@ export async function GET(){
     const API_KEY = import.meta.env.LASTFM_API_KEY;    
     const url = `${API_URL}?method=user.getrecenttracks&user=noel_30_tom&api_key=${API_KEY}&format=json&limit=1`;
     try{
-        const response = await fetch(url);
+        const response = await fetch(url,{
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "User-Agent":"personalwebsite/1.0"
+            }
+        }
+        );
         if(!response.ok)
             throw new Error(`Failed to fetch: ${response.statusText}`);            
         return response;
