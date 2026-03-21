1dcde6d684d9dfc53fbc033d7ad9e0af7f9d9da5
noeltom30 <noel.2005.tom@gmail.com>
2026-03-21 12:36:28 +0530

    got the keep api working

---
+++ src/pages/api/keep.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from "astro";
+import { env } from "cloudflare:workers";
+
+export const GET: APIRoute = async () => {
+    
+    const db = env.keep_database;
+    const { results } = await db.prepare("Select * from keep").all();
+    return Response.json(results, { status: 200 });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+    const db = env.keep_database;
+    const { name, message } = await request.json();
+
+    if (!name || !message) {
+        return new Response("Missing name or message", { status: 400 });
+    }
+
+    await db
+        .prepare("INSERT INTO keep (name, message) VALUES (?1, ?2)")
+        .bind(name, message)
+        .run();
+
+    return new Response(null, { status: 204 });
+};
